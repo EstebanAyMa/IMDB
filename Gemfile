89256572e4be77c3e4cfed905d87e0deafa43493
aayalahernandez <arturo_ayala@apple.com>
2021-11-08 20:03:47 -0800

    New fields

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mongoid', '~> 7.0.5'
+
+gem 'slim'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,18 @@
 class Post
   include Mongoid::Document
   field :title, type: String
-  field :detail, type: String
-  field :genre, type: String
-  field :duration, type: String
-
+  field :thumbnail_url, type: String
+  # field :detail, type: String
+  # field :release_date, type: DateTime
+  # field :movie_type, type: String
+  # field :director, type: String
+  # field :producer, type: String
+  # field :language, type: String
+  # field :genre, type: String
+  # field :duration, type: String
+  # field :rated, type: String
+  # field :media_formats, type: String
+  # field :origin_countries, type: String
+  # field :production_companies, type: String
+  # field :distributors, type: String
 end
